ab18019c256545019dba5450176777fd82097f6f
elohimproject <87337323+elohimproject@users.noreply.github.com>
2024-10-15 23:44:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,97 @@
-class Card {
-    constructor(suit, rank, value) {
-        this.suit = suit;
-        this.rank = rank;
-        this.value = value;
-    }
-}
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "A",
+    value: 1
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "2",
+    value: 2
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "3",
+    value: 3
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "4",
+    value: 4
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "5",
+    value: 5
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "6",
+    value: 6
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "7",
+    value: 7
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "8",
+    value: 8
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "9",
+    value: 9
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "10",
+    value: 10
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "J",
+    value: 11
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades", "diamonds", "hearts",
+    rank: "Q",
+    value: 12
+};
+console.log(card);
+
+let card = {
+    suit: "clubs","spades",
+    rank: "K",
+    value: 13
+};
+console.log(card);
+
+let card = {
+    suit: "diamonds", "hearts",
+    rank: "K",
+    value: 0
+};
+console.log(card);
